5da9d4a54a1baed96231484212b97bfb35c2d648
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-29 20:30:59 -0700

    updated brick functionality (collisions and deletion)

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ var brickRowCount = 3,
 
 
 var bricks = [];
-
 for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (var r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -49,6 +48,25 @@ function keyUpHandler(e) {
     }
 }
 
+function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status === 1) {
+                //calculations - all four statements must be true
+                // The position of ball x > positon of brick x
+                // The position of ball x < position of brick x + brickWidth
+                // The position of ball y > postion of brick y
+                // The position of ball y < position of brick y + brickHeight
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -68,15 +86,17 @@ function drawPaddle() {
 function drawBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
-            var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-            var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks[c][r].status === 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -87,6 +107,7 @@ function draw() {
     drawBall();
     drawPaddle();
     drawBricks();
+    collisionDetection();
     x += dx;
     y += dy;
 
