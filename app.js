2e3adb1956d6bf84fa406a404003adf509909229
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-29 18:37:16 -0700

    ball creation and inital movement

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas"),
+    ctx = canvas.getContext("2d"),
+    x = canvas.width / 2,
+    y = canvas.height - 30,
+    dx = -6,
+    dy = -5;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    // drawing code
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 15);
